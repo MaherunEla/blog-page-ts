052dcda187ed3ab439481d10ddfd703dac27013d
Maherun Nessa <meherunela2002@gmail.com>
2023-10-15 17:49:57 +0600

    shadcn

---
+++ src/app/author/[slug]/Components/DialogDemoTage.tsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export function DialogDemoTage() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Add Tage</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Add Tage for your blog</DialogTitle>
+          <DialogDescription>
+            Make changes to Category here. Click save when you&apos;re done.
+          </DialogDescription>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+              Category Name
+            </Label>
+            <Input
+              id="name"
+              defaultValue="Pedro Duarte"
+              className="col-span-3"
+            />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Save changes</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/app/author/[slug]/newblog/page.tsx
+++ src/app/author/[slug]/newblog/page.tsx
@@ -1,43 +1,171 @@
 "use client";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
+import { Switch } from "@/components/ui/switch";
 import React from "react";
 import { Editor } from "novel";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { DialogDemo } from "../Components/DialogDemo";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
 const NewBlogpage = () => {
-  const form = useForm<FormValues>();
+  const createBlogFormSchema = z.object({
+    title: z.string(),
+    backgroundImage: z.string(),
+    categoryname: z.string(),
+    tagname: z
+      .array(
+        z.object({
+          value: z.string(),
+        })
+      )
+      .optional(),
+  });
+  type FormValues = z.infer<typeof createBlogFormSchema>;
 
-  type FormValues = {
-    title: string;
-    backgroundImage: string;
+  const defaultValues: Partial<FormValues> = {
+    title: "This is my first Blog",
+    tagname: [{ value: "Data" }, { value: "Technology" }],
   };
-  const { register, handleSubmit } = form;
+  const form = useForm<FormValues>({
+    resolver: zodResolver(createBlogFormSchema),
+    defaultValues,
+    mode: "onChange",
+  });
+
+  const { register, control, handleSubmit } = form;
+  const { fields, append } = useFieldArray({
+    name: "tagname",
+    control,
+  });
   const onSubmit = (data: FormValues) => {
     console.log("form Submited", data);
   };
   return (
-    <div>
-      <div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" {...register("title")} />
-          <label htmlFor="backgroundImage">Background Image</label>
-          <input
-            type="file"
-            id="backgroundImage"
-            {...register("backgroundImage")}
+    <div className="container py-10">
+      <div className="flex flex-col gap-5">
+        <Form {...form}>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Title</FormLabel>
+                <FormControl>
+                  <Input placeholder="Enter Blog Title" {...field} />
+                </FormControl>
+                <FormDescription>This is your Blog Title.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-          <label htmlFor="backgroundImage">Background Image</label>
-          <input
-            type="file"
-            id="backgroundImage"
-            {...register("backgroundImage")}
+          <FormField
+            control={form.control}
+            name="backgroundImage"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Background Image</FormLabel>
+                <FormControl>
+                  <Input type="file" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your Blog Background Image.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
           />
-        </form>
+
+          <FormField
+            control={form.control}
+            name="categoryname"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Select Category</FormLabel>
+                <FormControl>
+                  <Select>
+                    <SelectTrigger className="w-[180px]">
+                      <SelectValue placeholder="Select Category" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectItem value="light">Light</SelectItem>
+                      <SelectItem value="dark">Dark</SelectItem>
+                      <SelectItem value="system">System</SelectItem>
+                      <DialogDemo />
+                    </SelectContent>
+                  </Select>
+                </FormControl>
+                <FormDescription>This is your Blog Category.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="border border-red-500 flex items-center justify-center gap-2">
+            {fields.map((field, index) => (
+              <FormField
+                control={form.control}
+                key={field.id}
+                name={`tagname.${index}.value`}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormDescription>{field.value}</FormDescription>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            ))}
+
+            <Button
+              type="button"
+              variant="outline"
+              size="sm"
+              className=""
+              onClick={() => append({ value: "" })}
+            >
+              Add Tage
+            </Button>
+          </div>
+          <Label htmlFor="description">Description</Label>
+          <Editor
+            onUpdate={(data) => {
+              console.log(data?.getHTML());
+            }}
+          />
+
+          <div className="flex items-center space-x-2">
+            <Switch id="airplane-mode" />
+            <Label htmlFor="feature">Feature Blog</Label>
+          </div>
+        </Form>
       </div>
-      <Editor
-        onUpdate={(data) => {
-          console.log(data?.getHTML());
-        }}
-      />
     </div>
   );
 };
